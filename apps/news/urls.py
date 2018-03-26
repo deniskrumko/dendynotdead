efa2883e4b1509558edb5971d6acc9416dfd78cd
Denis Krumko <dkrumko@gmail.com>
2018-03-26 16:53:10 +0300

    Remove unused files

--- apps/news/urls.py
+++ apps/news/urls.py
@@ -4,5 +4,7 @@ from .views import NewsView, SingleNewsView
 
 urlpatterns = [
     url('^$', NewsView.as_view(), name='news'),
-    url('^(?P<page_slug>[\w-]+)$', SingleNewsView.as_view(), name='single-news'),
+    url(
+        '^(?P<page_slug>[\w-]+)$', SingleNewsView.as_view(), name='single-news'
+    ),
 ]
